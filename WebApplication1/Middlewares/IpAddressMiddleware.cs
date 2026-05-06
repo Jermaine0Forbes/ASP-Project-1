c6617a4fd85d523f3b85a8464a36634000d9ecc3
Jermaine Forbes <jermaine0forbes@gmail.com>
2026-05-06 17:34:34 -0400

    implement ip tracking pt3

--- WebApplication1/Middlewares/IpAddressMiddleware.cs
+++ WebApplication1/Middlewares/IpAddressMiddleware.cs
@@ -1,37 +1,70 @@
-public class IpAddressMiddleware
+using IP2LocationIOComponent;
+using System.Net.Http;
+
+namespace WebApplication1.Middlewares
 {
-    private readonly RequestDelegate _next;
-    private readonly string[] _ignoreExtensions = [".js", ".ts", ".json", ".css", ".scss", ".png", ".jpg", ".map", ".ico"];
-
-    public IpAddressMiddleware(RequestDelegate next)
+    public class IpAddressMiddleware
     {
-        _next = next;
-    }
+        private readonly RequestDelegate _next;
+        private readonly string[] _ignoreExtensions = [".js", ".ts", ".json", ".css", ".scss", ".png", ".jpg", ".map", ".ico"];
+        private readonly string[] _ignoreIps = ["::1", "127.0.0.1"];
 
-    private Boolean isEndPoint(HttpContext context)
-    {
-        return !_ignoreExtensions.Any(ext => 
-        context.Request.Path.Value!=null ? 
-        context.Request.Path.Value.EndsWith(ext, StringComparison.OrdinalIgnoreCase) 
-        : false
-        );
-    }
+        public IpAddressMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
 
-    public async Task InvokeAsync(HttpContext context)
-    {
+        private bool isEndPoint(HttpContext context)
+        {
+            return !_ignoreExtensions.Any(ext =>
+            context.Request.Path.Value != null ?
+            context.Request.Path.Value.EndsWith(ext, StringComparison.OrdinalIgnoreCase)
+            : false
+            );
+        }
 
-        if(isEndPoint(context))
+        private async Task<string> GetIp(HttpContext context)
         {
             // Access the client's IP address
             var remoteIp = context.Connection.RemoteIpAddress?.ToString();
+            var ignoreIp = _ignoreIps.Any(ip => ip == remoteIp);
+            if (ignoreIp)
+            {
+                using var client = new HttpClient();
+                string publicIp = await client.GetStringAsync("https://api.ipify.org");
 
-            // Example: Add the IP to response headers
-            context.Response.Headers.Append("X-Client-IP", remoteIp);
-            Console.WriteLine("Path is {0}", context.Request.Path);
-            Console.WriteLine("Ip address is {0}", remoteIp);
-            
+                return publicIp;
+            }
+
+            return remoteIp ?? "";
         }
 
-        await _next(context);
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            if (isEndPoint(context))
+            {
+                // Configures IP2Location.io API key
+                Configuration Config = new()
+                {
+                    ApiKey = "E9A1ADCCF63B2DFF08FF5854D511FABE"
+                };
+                IPGeolocation IPL = new(Config);
+                var remoteIp = await GetIp(context);
+
+                var endpoint = await IPL.Lookup(remoteIp);
+
+                // Example: Add the IP to response headers
+                context.Response.Headers.Append("X-Client-IP", remoteIp);
+                Console.WriteLine("Path is {0}", context.Request.Path);
+                Console.WriteLine("Ip address is {0}", remoteIp);
+                Console.WriteLine("endpoint {0}", endpoint["country_name"]);
+
+            }
+
+            await _next(context);
+        }
     }
+
+
 }
